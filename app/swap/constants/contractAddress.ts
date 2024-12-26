e524264d1879afe636a874e5a3617004dba3a1b9
ntnam-bic <ngothanhnam@evol.vn>
2024-12-26 17:07:46 +0700

    test: pool 1ETH

--- app/swap/constants/contractAddress.ts
+++ app/swap/constants/contractAddress.ts
@@ -3,13 +3,13 @@ export const NULL_ADDRESS: `0x${string}` = '0x0000000000000000000000000000000000
 export const EVM_CONTRACT: { [key: string]: {[key: string]: `0x${string}`} } = {
     1301: {
         UniswapRouter: '0x920b806E40A00E02E7D2b94fFc89860fDaEd3640',
-        BTest: '0xD9bC28C213d037BDd34308ED1e7Ce2cc3F5bA1e8',
-        Pair: '0xE7Df4d7870d57a08AAFb4c0949cfe33A0b454d91'
+        BTest: '0xA4e6267D961a6C818FBF038713A8cc071562dC9e',
+        Pair: '0x0a1d695bD7EaBA89EdeCf9A3Cd9D969058D6f012'
     },
     421614: {
         UniswapRouter: '0x920b806E40A00E02E7D2b94fFc89860fDaEd3640',
         BTest: '0xD9bC28C213d037BDd34308ED1e7Ce2cc3F5bA1e8',
-        Pair: '0xE7Df4d7870d57a08AAFb4c0949cfe33A0b454d91'
+        Pair: '0x0a1d695bD7EaBA89EdeCf9A3Cd9D969058D6f012'
     },
     42161: {
         AccessManager: '0x',
