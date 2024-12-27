26c68718df39f0691f6aa759666f5ed51360a843
ntnam-bic <ngothanhnam@evol.vn>
2024-12-27 15:47:33 +0700

    embed chart

--- app/swap/components/Swap.tsx
+++ app/swap/components/Swap.tsx
@@ -321,6 +321,9 @@ export function Swap() {
           <ExternalLink icon={true} href={`https://sepolia.uniscan.xyz/token/${EVM_CONTRACT[DEFAULT_CHAINID].Pair}`}>Accumulated Liquidity Position</ExternalLink>
         </div>
       </div>
+      <div className="bg-foreground border border-border-secondary p-6 w-full rounded-[10px]" style={{ height: '1000px'}}>
+        <iframe height="100%" width="100%" id="geckoterminal-embed" title="GeckoTerminal Embed" src="https://www.geckoterminal.com/unichain-sepolia/pools/0x0a1d695bd7eaba89edecf9a3cd9d969058d6f012?embed=1&info=0&swaps=1&grayscale=1&light_chart=0" frameBorder="0" allow="clipboard-write" allowFullScreen></iframe>
+      </div>
     </>
   );
 }
