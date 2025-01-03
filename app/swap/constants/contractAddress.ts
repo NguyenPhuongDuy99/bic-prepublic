158cf4c6cb18359272cb81baf470419dbb8ea3d9
ntnam-bic <ngothanhnam@evol.vn>
2025-01-03 17:55:24 +0700

    revert address on unichain sepolia

--- app/swap/constants/contractAddress.ts
+++ app/swap/constants/contractAddress.ts
@@ -6,8 +6,8 @@ export const NULL_ADDRESS: `0x${string}` = '0x0000000000000000000000000000000000
 export const EVM_CONTRACT: { [key: string]: {[key: string]: `0x${string}`} } = {
     1301: {
         UniswapRouter: '0x920b806E40A00E02E7D2b94fFc89860fDaEd3640',
-        BTest: '0xA4e6267D961a6C818FBF038713A8cc071562dC9e',
-        Pair: '0x0a1d695bD7EaBA89EdeCf9A3Cd9D969058D6f012'
+        BTest: '0x8955Cea6d5d9a7eD18C44cF634dCd1b2F64af783',
+        Pair: '0xe266048E8f5A1799fc1839C267293d6930a99Cfb'
     },
     421614: {
         UniswapRouter: '0x920b806E40A00E02E7D2b94fFc89860fDaEd3640',
