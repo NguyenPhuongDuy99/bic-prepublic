65dd9d1574f438d345b59adceae9a209454be191
Duy Nguyễn <nguyenduy30234@gmail.com>
2025-02-27 08:45:33 +0700

    feat: BEIN-32506 update UI mobile

--- app/stake/widgets/StakeTopDesktop.tsx
+++ app/stake/widgets/StakeTopDesktop.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Typograhphy from "@/components/Typograhphy";
 import { Card, CardContent } from "@/components/ui/card";
 import {
@@ -9,6 +10,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { cn } from "@/lib/utils";
+import { Button } from "@beincom/web-ui";
 import React from "react";
 
 interface StakeTopDesktop extends React.HTMLAttributes<HTMLDivElement> {}
@@ -45,6 +47,67 @@ const StakeTopDesktop = ({ className, ...props }: StakeTopDesktop) => {
       interest: "90,000 BIC (40%)",
       timeLeft: "90 days, 6 hours, 35 min",
     },
+    {
+      tier: 1,
+      stakingAmount: "1,000,000 BIC",
+      interest: "90,000 BIC (40%)",
+      timeLeft: "90 days, 6 hours, 35 min",
+    },
+    {
+      tier: 2,
+      stakingAmount: "1,000,000 BIC",
+      interest: "90,000 BIC (40%)",
+      timeLeft: "90 days, 6 hours, 35 min",
+    },
+    {
+      tier: 3,
+      stakingAmount: "1,000,000 BIC",
+      interest: "90,000 BIC (40%)",
+      timeLeft: "90 days, 6 hours, 35 min",
+    },
+    {
+      tier: 4,
+      stakingAmount: "1,000,000 BIC",
+      interest: "90,000 BIC (40%)",
+      timeLeft: "90 days, 6 hours, 35 min",
+    },
+    {
+      tier: 5,
+      stakingAmount: "1,000,000 BIC",
+      interest: "90,000 BIC (40%)",
+      timeLeft: "90 days, 6 hours, 35 min",
+    },
+    {
+      tier: 1,
+      stakingAmount: "1,000,000 BIC",
+      interest: "90,000 BIC (40%)",
+      timeLeft: "90 days, 6 hours, 35 min",
+    },
+    {
+      tier: 2,
+      stakingAmount: "1,000,000 BIC",
+      interest: "90,000 BIC (40%)",
+      timeLeft: "90 days, 6 hours, 35 min",
+    },
+    {
+      tier: 3,
+      stakingAmount: "1,000,000 BIC",
+      interest: "90,000 BIC (40%)",
+      timeLeft: "90 days, 6 hours, 35 min",
+    },
+    {
+      tier: 4,
+      stakingAmount: "1,000,000 BIC",
+      interest: "90,000 BIC (40%)",
+      timeLeft: "90 days, 6 hours, 35 min",
+    },
+    {
+      tier: 5,
+      stakingAmount: "1,000,000 BIC",
+      interest: "90,000 BIC (40%)",
+      timeLeft: "90 days, 6 hours, 35 min",
+      isTimeover: true,
+    },
   ];
   return (
     <Card className={cn("w-full rounded-xl", className)} {...props}>
@@ -59,25 +122,38 @@ const StakeTopDesktop = ({ className, ...props }: StakeTopDesktop) => {
               <TableHead>Time left</TableHead>
             </TableRow>
           </TableHeader>
-          <TableBody>
-            {dataTable.map((data) => (
-              <TableRow key={data.tier}>
-                <TableCell className="text-neutral-60 text-base font-normal leading-6">
-                  {data.tier}
-                </TableCell>
-                <TableCell className="text-neutral-60 text-base font-normal leading-6">
-                  {data.stakingAmount}
-                </TableCell>
-                <TableCell className="text-neutral-60 text-base font-normal leading-6">
-                  {data.interest}
-                </TableCell>
-                <TableCell className="text-neutral-60 text-base font-normal leading-6">
-                  {data.timeLeft}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
         </Table>
+
+        <div
+          className={cn({ "max-h-72 overflow-y-auto": dataTable.length > 10 })}
+        >
+          <Table>
+            <TableBody>
+              {dataTable.map((data, id) => (
+                <TableRow key={id}>
+                  <TableCell className="w-[58px] text-neutral-60 text-base font-normal leading-6">
+                    {data.tier}
+                  </TableCell>
+                  <TableCell className=" w-[396px] text-neutral-60 text-base font-normal leading-6">
+                    {data.stakingAmount}
+                  </TableCell>
+                  <TableCell className="w-[396px] text-neutral-60 text-base font-normal leading-6">
+                    {data.interest}
+                  </TableCell>
+                  <TableCell className="text-neutral-60 text-base font-normal leading-6">
+                    {data.isTimeover ? (
+                      <Button onClick={() => console.log("click")}>
+                        Withdraw
+                      </Button>
+                    ) : (
+                      data.timeLeft
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
       </CardContent>
     </Card>
   );
--- app/stake/widgets/StakeTopMobile.tsx
+++ app/stake/widgets/StakeTopMobile.tsx
@@ -1,3 +1,4 @@
+import StakingMobileItem from "@/app/stake/widgets/StakingMobileItem";
 import Typograhphy from "@/components/Typograhphy";
 import { Card, CardContent } from "@/components/ui/card";
 import {
@@ -44,20 +45,36 @@ const StakeTopMobile = ({ className, ...props }: StakeTopMobile) => {
       stakingAmount: "1,000,000 BIC",
       interest: "90,000 BIC (40%)",
       timeLeft: "90 days, 6 hours, 35 min",
+      isTimeOver: true,
     },
   ];
   return (
     <Card className={cn("w-full rounded-xl", className)} {...props}>
       <CardContent className="p-6">
         <Typograhphy className="mb-5">My staking</Typograhphy>
-        <div className="">
-          <h3>Tier 1</h3>
-          <div className="flex flex-col gap-3">
-            <div className="flex justify-between">
-              <p className="text-neutral-30 text-xs leading-5">Staking amount</p>
-              <p className="text-neutral-60 text-sm leading-5">1,000,000 BIC</p>
+        <div className="flex flex-col gap-4">
+          {dataTable.map((item) => (
+            <div className="[&:not(:first-child)]:border-t-gray-5 [&:not(:first-child)]:border-t-[1px] [&:not(:first-child)]:pt-4">
+              <h3 className="text-neutral-60 text-sm font-semibold">
+                Tier {item.tier}
+              </h3>
+              <div className="flex flex-col gap-3">
+                <StakingMobileItem
+                  title="Staking Amount"
+                  value={item.stakingAmount}
+                />
+                <StakingMobileItem
+                  title="Interest (ARP)"
+                  value={item.interest}
+                />
+                <StakingMobileItem
+                  title="Time left"
+                  value={item.timeLeft}
+                  isTimeOver={item.isTimeOver}
+                />
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </CardContent>
     </Card>
